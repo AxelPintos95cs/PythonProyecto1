d1443bace16ca2095d273058da779f4c73e0ef4b
Axel <axel95.cs@hotmail.com>
2024-11-29 15:57:20 -0300

    Version 0.3 - Tests

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'myapp',
+    'Main',
     'blog',
 ]
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
